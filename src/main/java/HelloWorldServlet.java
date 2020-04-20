9125866a0a98166a10520f55739b1ed9378dcb75
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-04-20 16:46:44 -0500

    count.java

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,27 @@
+//import org.graalvm.compiler.serviceprovider.ServiceProvider;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    public String name = "";
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        String name = request.getParameter("name");
+        PrintWriter out = response.getWriter();
+
+        if (name != null) {
+            out.println("<h1>Hello, World!" + name + "</h1>");
+        } else {
+            out.println("<h1>Hello, world!</h1>");
+        }
     }
 }
