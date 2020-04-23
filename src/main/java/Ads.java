206f4f16276c71c3f36f5dfdbf16cc8ab82a3365
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-04-22 22:02:40 -0500

    ads

--- src/main/java/Ads.java
+++ src/main/java/Ads.java
@@ -1,50 +1,8 @@
-import java.io.Serializable;
+public interface Ads {
+    //returns object of type "Ad"
+    //searched by "id"
+    Ad findById(long id);
 
-//Ads Java bean
-public class Ads implements Serializable {
-    //Serializable allows string representation
-
-    private long id;
-
-    private String title;
-
-    private long priceInCents;
-
-    private String description;
-
-    //zero-argument constructor ; preserving space in memory
-    public Ads(){}
-
-    //getters & setters
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public long getPriceInCents() {
-        return priceInCents;
-    }
-
-    public void setPriceInCents(long priceInCents) {
-        this.priceInCents = priceInCents;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    //below will add "Ad" into table
+    long createAd(Ad ad);
 }
