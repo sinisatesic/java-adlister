90bc3601347a6d229903a6e48be08b2550510ccc
Sinisa Tesic <sinisa.tesic210@gmail.com>
2020-05-07 19:09:14 -0500

    view ad servlet added

--- src/main/java/com/codeup/adlister/controllers/ViewAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewAdServlet.java
@@ -1,6 +1,5 @@
 package com.codeup.adlister.controllers;
 
-
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.Ad;
 
@@ -10,25 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-@WebServlet("/ads/id")
+@WebServlet("/ads/view")
 public class ViewAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String idStr = request.getParameter("id");
-        int id = Integer.parseInt(idStr);
+        long id = Long.parseLong(idStr);
 
-        //this is creating a list of ads from one ad to see that it works
         Ad ad = DaoFactory.getAdsDao().getById(id);
-        List<Ad> fake = new ArrayList<>();
-        fake.add(ad);
-        System.out.println(ad.getId());
-        System.out.println(ad.getTitle());
-        System.out.println(ad.getDescription());
 
-        request.setAttribute("ads", fake);
+        request.setAttribute("ad", ad);
         System.out.println(ad);
-        request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/ads/view.jsp").forward(request, response);
     }
 }
